3023678f3a489463f9fdca0ace653f2ca1dcc2ff
VladSkliar <vlad.skliar@gmail.com>
2017-02-03 17:29:53 +0200

    Add db credentials

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web:  gunicorn app:app --worker-class eventlet -w 3
+init: python models.py
--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from playhouse.pool import PooledPostgresqlExtDatabase
 
 # database = PostgresqlDatabase('chat_project', user='chat_root_user', password='qwerty',)
 
-db = PooledPostgresqlExtDatabase(database="chat_project", user="chat_root_user",
-                                 password="qwerty", host="localhost", port=5432,
+db = PooledPostgresqlExtDatabase(database="debfucbpr7af0b", user="iqinufrugqclnw",
+                                 password="03f771066cfbf4d40a2708223aed92bf1814ec3704237851bab66d051e177f3d", host="ec2-54-163-240-7.compute-1.amazonaws.com", port=5432,
                                  register_hstore=False, autorollback=True)
 db.commit_select = True
 db.autorollback = True
@@ -33,3 +33,6 @@ def init_db():
     except:
         db.rollback()
         raise
+
+if __name__ == '__main__':
+    init_db()
